4990a7749756cc85bae6d1b1c00146a3c24cc501
jtomas21 <127454496+jtomas21@users.noreply.github.com>
2023-03-27 18:46:59 -0300

    agregue titulo nuevo

---
+++ algoritmo.js
@@ -0,0 +1,11 @@
+document.querySelector("body").innerHTML =`
+<details>
+        <summary>repaso</summary>
+        <img src="https://previews.123rf.com/images/get4net/get4net1010/get4net101000384/8112578-ilustración-de-mariposa-formada-por-varias-mariposas-coloridas.jpg"
+            width="620px" height="500px" alt="imagen varias">
+        <iframe width="300px" height="300px" src="https://es.wikipedia.org/wiki/Wiki" frameborder="0"></iframe>
+        <p>Netflix tiene un amplio catálogo de películas, series, documentales, animes, originales premiados y más. Todo
+            lo que quieras ver, cuando quieras.</p>
+            <a href="https://www.netflix.com/ar/">ver pelis</a>
+    </details>
+`;
